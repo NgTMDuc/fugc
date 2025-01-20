2b37ee85ccd869d75457fbd7fbc69107c1ba0505
NgTMDuc <ducntm23052002@gmail.com>
2025-01-20 18:02:09 +0700

    Running

---
+++ load_data.py
@@ -0,0 +1,34 @@
+
+from datasets import load_dataset 
+import torch
+from torch.utils.data import Dataset, DataLoader
+from transformers import BertTokenizer, BertModel, AdamW, get_linear_schedule_with_warmup
+from sklearn.metrics import accuracy_score, classification_report 
+
+class CustomDataset(Dataset):
+    def __init__(self, name, tokenizer, max_length = 512, split = "train"):
+        super().__init__()
+
+        self.name = name
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+        print(name)
+        self.data = load_dataset(name, split)
+    
+    def getitem(self, index):
+        text = self.data["sentence"][index]
+        inputs = self.tokenizer(text, padding="max_length", truncation=True, max_length=self.max_length)
+
+        return {
+            "input_ids": torch.tensor(inputs["input_ids"], dtype=torch.long),
+            "attention_mask": torch.tensor(inputs["attention_mask"], dtype=torch.long),
+            "label": torch.tensor(self.data["label"], dtype=torch.long)
+        }
+    
+def dataloader(cfg, split = "train"):
+    tokenizer = BertTokenizer.from_pretrained(cfg["MODEL"]["BACKBONE"])
+    dataset = CustomDataset(cfg["DATA"]["NAME"], tokenizer, max_length = cfg["DATA"]["MAX_LENGTH"], split = split)
+    dataloader = DataLoader(dataset, batch_size = cfg["DATA"]["BATCH_SIZE"])
+
+    return dataloader
+
---
+++ main.py
@@ -0,0 +1,69 @@
+from load_data import dataloader
+from load_config import load_config
+from tqdm import tqdm 
+import torch 
+from loss import SoftTriple
+from sklearn.metrics import accuracy_score, classification_report
+from model import BERTClassifier
+from torch import nn
+from transformers import AdamW, get_linear_schedule_with_warmup
+def train(model, dataloader, optimizer, scheduler, device, propose_loss = None, alpha = 0):
+    model.train()
+    for batch in tqdm(dataloader):
+        optimizer.zero_grad()
+        input_ids = batch['input_ids'].to(device)
+        attention_mask = batch['attention_mask'].to(device)
+        labels = batch['label'].to(device)
+        outputs = model(input_ids=input_ids, attention_mask=attention_mask).softmax(1)
+        if propose_loss is None:
+            loss = nn.CrossEntropyLoss()(outputs, labels)
+        else:
+            loss = alpha * nn.CrossEntropyLoss()(outputs, labels) + (1 - alpha) * propose_loss(outputs, labels)
+
+        loss.backward()
+        optimizer.step()
+        scheduler.step()
+    
+
+def evaluate(model, dataloader, device):
+    model.eval()
+    predictions = []
+    actual_labels = []
+
+    with torch.no_grad():
+        for batch in dataloader:
+            input_ids = batch['input_ids'].to(device)
+            attention_mask = batch['attention_mask'].to(device)
+            labels = batch['label'].to(device)
+            outputs = model(input_ids=input_ids, attention_mask=attention_mask)
+            _, preds = torch.max(outputs, dim=1)
+            predictions.extend(preds.cpu().tolist())
+            actual_labels.extend(labels.cpu().tolist())
+    return accuracy_score(actual_labels, predictions), classification_report(actual_labels, predictions)
+
+if __name__ == "__main__":
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    PATH = "config.yaml"
+
+    cfg = load_config(PATH)
+    train_loader = dataloader(cfg, split = "train")
+    valid_dataset  = dataloader(cfg, split = "validation")
+    model = BERTClassifier(cfg["MODEL"]["NAME"], 2)
+    optimizer = AdamW(model.parameters(), lr=cfg["OPTIMIZER"]["LR"])
+    total_steps = len(train_loader) * cfg["OPTIMIZER"]["NUM_EPOCHS"]
+
+    scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=0, num_training_steps=total_steps)
+
+
+    if cfg["OPTIMIZER"]["PROPOSE"]:
+        proposal_criteria = SoftTriple(cfg["OPTIMIZER"]["la"], cfg["OPTIMIZER"]["gamma"], cfg["OPTIMIZER"]["tau"],
+                                        cfg["OPTIMIZER"]["margin"], model.config.hidden_size, 2, cfg["OPTIMIZER"]["NUMBER_K"],device = device)
+    
+    else:
+        proposal_criteria = None
+    for epoch in range(cfg["OPTIMIZER"]["NUM_EPOCHS"]):
+        print(f"Epoch {epoch + 1}")
+        train(model, train_loader, optimizer, scheduler, device = device, propose_loss = proposal_criteria, alpha = cfg["OPTIMIZER"]["alpha"])
+        accuracy, report = evaluate(model, valid_dataset, device)
+        print(f"Validation Accuracy: {accuracy:.4f}")
+        print(report)
